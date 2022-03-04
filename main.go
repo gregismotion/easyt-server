7dfe06fe67458b0a584ad79e5c1f55460d65e55d
thegergo02 <thegergo02@freeself.one>
2022-03-04 06:20:09 +0100

    fix: better error handling

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	//"fmt"
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"bytes"
+	"encoding/json"
 )
 
 func main() {
@@ -44,13 +46,45 @@ func getData(c *gin.Context) {}
 func deleteData(c *gin.Context) {}
 
 type NamedType struct {
-	name string
-	typ BasicType
+	Name string `json:"name"`
+	Type BasicType `json:"type"`
 }
+func isNamedTypeUnique(namedType NamedType, namedTypes []NamedType) bool {
+	for _, elem := range namedTypes {
+		if elem.Name == namedType.Name {
+			return false
+		}
+	}
+	return true
+}
+var namedTypes = make([]NamedType, 0)
 
-//var namedTypes = make([]NamedType)
-func getNamedTypes(c *gin.Context) {}
-func createNamedType(c *gin.Context) {}
+func getNamedTypes(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, namedTypes)
+}
+func createNamedType(c *gin.Context) {
+	typ, ok := strToBasicType(c.PostForm("type"))
+	if ok {
+		name := c.PostForm("name")
+		if name != "" {
+			namedType := NamedType {
+				Name: name,
+				Type: typ,
+			}
+			ok = isNamedTypeUnique(namedType, namedTypes)
+			if ok {
+				namedTypes = append(namedTypes, namedType)
+				c.IndentedJSON(http.StatusOK, namedType)
+			} else {
+				c.IndentedJSON(http.StatusBadRequest, gin.H{"error": "Duplicate name!"})
+			}
+		} else {
+			c.IndentedJSON(http.StatusBadRequest, gin.H{"error": "No name specified!"})
+		}
+	} else {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"error": "Unknown basic type!"})
+	}
+}
 func getNamedType(c *gin.Context) {}
 func deleteNamedType(c *gin.Context) {}
 
@@ -59,17 +93,45 @@ const (
 	num BasicType = iota
 	str
 )
-var basicTypes = []string{"num", "str"}
-func (t BasicType) String() string {
-	switch t {
-		case num:
-			return "num"
-		case str:
-			return "str"
+// NOTE: this will get out of hand, FIND ALTERNATIVE!!!
+var strToBasicTypes = map[string]BasicType {
+	"num": num,
+	"str": str,
+}
+var basicTypesToStr = map[BasicType]string {
+	num: "num",
+	str: "str",
+}
+func (t BasicType) String() (str string) {
+	str, ok := basicTypesToStr[t]
+	if !ok {
+		str = "unknown"
 	}
-	return "unknown"
+	return
+}
+func strToBasicType(str string) (BasicType, bool) {
+	typ, ok := strToBasicTypes[str]
+	return typ, ok
+}
+func (typ BasicType) MarshalJSON() ([]byte, error) {
+	buffer := bytes.NewBufferString(`"`)
+	buffer.WriteString(typ.String())
+	buffer.WriteString(`"`)
+	return buffer.Bytes(), nil
+}
+func (typ *BasicType) UnmarshalJSON(b []byte) error {
+	var j string
+	err := json.Unmarshal(b, &j)
+	if err != nil {
+		return err
+	}
+	readTyp, ok := strToBasicType(j)
+	if ok {
+		*typ = readTyp
+	} 	
+	return nil
 }
 
 func getBasicTypes(c *gin.Context) {
-	c.IndentedJSON(http.StatusOK, basicTypes)
+	c.IndentedJSON(http.StatusOK, strToBasicTypes)
 }
