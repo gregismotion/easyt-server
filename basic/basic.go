60c56c6ab6f28b8e8b4e2b2f512ed5699d3c0f7b
thegergo02 <thegergo02@freeself.one>
2022-04-01 17:44:27 +0200

    refactor: remove unnec print

--- basic/basic.go
+++ basic/basic.go
@@ -2,7 +2,6 @@
 package basic
 
 import (
-	"fmt"
 	"encoding/json"
 	"bytes"
 )
@@ -46,7 +45,6 @@ func (t BasicType) String() (str string) {
 // Convert a string to it's BasicType counterpart
 func StrToBasicType(str string) (typ BasicType, ok bool) {
 	typ, ok = StrToBasicTypes[str]
-	fmt.Println(ok)
 	return
 }
 
